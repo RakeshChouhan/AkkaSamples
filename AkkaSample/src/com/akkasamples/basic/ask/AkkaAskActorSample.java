52ddba8a3a4efe01557d8832d43a5469a7734227
RakeshChouhan <rakesh.chouhan5j85@gmail.com>
2014-12-19 12:49:06 +0530

    Adding one more sample

---
+++ AkkaSample/src/com/akkasamples/basic/ask/AkkaAskActorSample.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.akkasamples.basic.ask;
+
+/**
+ * @author chouhan_r
+ *
+ */
+public class AkkaAskActorSample {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+	}
+
+}
---
+++ AkkaSample/src/com/akkasamples/basic/ask/MessageProviderActor.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.akkasamples.basic.ask;
+
+import akka.actor.UntypedActor;
+
+/**
+ * @author chouhan_r
+ *
+ */
+public class MessageProviderActor extends UntypedActor {
+
+	/* (non-Javadoc)
+	 * @see akka.actor.UntypedActor#onReceive(java.lang.Object)
+	 */
+	@Override
+	public void onReceive(Object msg) throws Exception {
+		
+	}
+
+}
