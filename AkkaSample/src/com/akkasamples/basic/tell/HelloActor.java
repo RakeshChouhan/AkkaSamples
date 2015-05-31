7f24ac4854a6871a0cc9cc548e348d5088b484f4
AkkaSample <rakesh.chouhan5j85@gmail.com>
2015-05-31 23:38:11 +0530

    Commented the file.

--- AkkaSample/src/com/akkasamples/basic/tell/HelloActor.java
+++ AkkaSample/src/com/akkasamples/basic/tell/HelloActor.java
@@ -1,13 +1,18 @@
 package com.akkasamples.basic.tell;
 import akka.actor.UntypedActor;
-
-public class HelloActor extends UntypedActor {
+/**
+ * 
+ * @author Rakesh
+ *
+ */
+public class HelloActor extends UntypedActor { // extends UntypedActor 
 
 	@Override
-	public void onReceive(Object msg) throws Exception {
-		if (msg instanceof String) {
+	public void onReceive(Object msg) throws Exception { // recieving the message from the AkkaWithMain.java.
+		if (msg instanceof String) { // checking the instance of recieved message.
 			System.out.println("Given String is :: " + msg);
 		}
+		unhandled(msg); // if msg is not String then it will be handled by the default handler.
 	}
 
 }
