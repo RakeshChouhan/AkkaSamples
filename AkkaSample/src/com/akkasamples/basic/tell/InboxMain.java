d1e000f6d4a5c5b557df056afd3f14bcdc26cc34
RakeshChouhan <rakesh.chouhan5j85@gmail.com>
2015-06-02 17:52:31 +0530

    Selection code added

--- AkkaSample/src/com/akkasamples/basic/tell/InboxMain.java
+++ AkkaSample/src/com/akkasamples/basic/tell/InboxMain.java
@@ -3,7 +3,11 @@
  */
 package com.akkasamples.basic.tell;
 
+import java.util.concurrent.TimeUnit;
+
+import scala.concurrent.duration.FiniteDuration;
 import akka.actor.ActorRef;
+import akka.actor.ActorSelection;
 import akka.actor.ActorSystem;
 import akka.actor.Inbox;
 import akka.actor.Props;
@@ -23,7 +27,11 @@ public class InboxMain {
 		ActorRef target = system.actorOf(Props.create(HelloActor.class),"HelloActor");
 		inbox.watch(target);
 		inbox.send(target, "Hello from inbox"); // send message from outside the actor system.
-
+		ActorSelection selection = system.actorSelection("/user/HelloActor");
+		
+		selection.tell("This is from selection", ActorRef.noSender());
+		selection.resolveOne(FiniteDuration.apply(10, TimeUnit.SECONDS));
+		
 		system.shutdown();
 	}
 
